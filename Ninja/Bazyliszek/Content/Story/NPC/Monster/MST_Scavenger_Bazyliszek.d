3b3f35d808206c5167042a3b684cbeacd61e8180
damianut <damianas1999@gmail.com>
2024-09-21 18:43:04 +0200

    Stats increased

--- Ninja/Bazyliszek/Content/Story/NPC/Monster/MST_Scavenger_Bazyliszek.d
+++ Ninja/Bazyliszek/Content/Story/NPC/Monster/MST_Scavenger_Bazyliszek.d
@@ -1,8 +1,13 @@
 instance Patch_Bazyliszek_Bazyliszek(Mst_Default_Scavenger_Demon)
 {
     name = "Bazyliszek";
+    level = 30;
+	attribute[ATR_STRENGTH] = 120;
+	attribute[ATR_DEXTERITY] = 120;
+	attribute[ATR_HITPOINTS_MAX] = 500;
+	attribute[ATR_HITPOINTS] = 500;
 	B_SetVisuals_Scavenger_Demon();
     Mdl_SetModelScale(self,2,2,2);
 	Npc_SetToFistMode(self);
-	CreateInvItems(self,ItFoMuttonRaw,1);
+	CreateInvItems(self,ItFoMuttonRaw,2);
 };
