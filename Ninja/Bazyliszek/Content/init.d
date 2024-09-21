f3cd762873e8567ce0334a3950e638c69a405033
damianut <41262420+damianut@users.noreply.github.com>
2024-09-21 16:31:55 +0000

    Initialize patch

---
+++ Ninja/Bazyliszek/Content/init.d
@@ -0,0 +1,8 @@
+/*
+ * Initialization function called by Ninja after "Init_Global" (G2) / "Init_<Levelname>" (G1)
+ */
+func void Ninja_Bazyliszek_Init() {
+
+    // WRITE YOUR INITIALIZATIONS HERE
+
+};
