6b47770db751d75465300f5bbc862cd6a8efd108
damianut <damianas1999@gmail.com>
2024-09-21 18:40:16 +0200

    Typo

--- Ninja/Bazyliszek/Content/Story/NPC/Monster/MST_Scavenger_Bazyliszek.d
+++ Ninja/Bazyliszek/Content/Story/NPC/Monster/MST_Scavenger_Bazyliszek.d
@@ -1,6 +1,6 @@
 instance Patch_Bazyliszek_Bazyliszek(Mst_Default_Scavenger_Demon)
 {
-    name = "Bazyliszek"
+    name = "Bazyliszek";
 	B_SetVisuals_Scavenger_Demon();
     Mdl_SetModelScale(self,2,2,2);
 	Npc_SetToFistMode(self);
